c1b64d1f114f054cf86258cd3e310374f1193287
wu <jianhengwu0407@gmail.com>
2022-07-27 01:39:58 +0800

    更新一些方法的文档说明

--- fastpt/utils/file_control.py
+++ fastpt/utils/file_control.py
@@ -4,6 +4,12 @@ import re
 
 
 def get_file_property(filepath: str) -> tuple:
+    """
+    获取文件属性
+
+    :param filepath:
+    :return:
+    """
     filename = re.split(r'/|\'|\\|\\\\', filepath)[-1]
     file_real_name = '.'.join(filename.split('.')[:-1])
     filetype = filename.split('.')[-1]
