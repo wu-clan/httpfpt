a1dd0cd790670ce8c68177259e0043e49f9688df
wu <jianhengwu0407@gmail.com>
2022-08-12 19:22:14 +0800

    更新请求数据架构

--- fastpt/schema/case_data.py
+++ fastpt/schema/case_data.py
@@ -1,56 +1,79 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from typing import Optional, Union, Tuple, List, Dict
+from typing import Optional, Union, Tuple, List, Dict, Literal
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator, AnyHttpUrl
 
 
-class ConfigAllureData(BaseModel):
+class _ConfigAllureData(BaseModel):
     epic: str
     feature: str
     story: str
 
+    @validator('*')
+    def check_allure_type(cls, v):
+        if not isinstance(v, str):
+            raise ValueError(f'测试用例数据解析失败, 参数 config:allure:{v} 不是有效的 str 类型')
+        return v
 
-class ConfigRequestData(BaseModel):
+
+class _ConfigRequest(BaseModel):
     env: str
     headers: Optional[dict] = None
     timeout: Optional[int] = None
     verify: Optional[bool] = None
     redirects: Optional[bool] = None
-    proxies: Optional[dict] = None
+    proxies: Union[Dict[Union[Literal['requests', 'httpx']], Dict[
+        Literal['http', 'https', 'http://', 'https://'], Union[AnyHttpUrl, None]]]] = None  # noqa
 
 
-class ConfigModuleData(BaseModel):
+class Config(BaseModel):
+    allure: _ConfigAllureData
+    request: _ConfigRequest
     module: str
 
 
-class StepsCaseInfoData(BaseModel):
+class _StepsRequestData(BaseModel):
+    method: str
+    url: str
+    params: Union[dict, bytes, None] = ...
+    headers: Optional[dict] = ...
+    data_type: Optional[str] = ...
+    data: Union[dict, bytes, Tuple[list], None] = ...
+    files: Union[Dict[str, Union[List[str], str]], None] = ...
+
+    @validator('headers')
+    def check_headers(cls, v):
+        if v is not None:
+            if not isinstance(v, dict):
+                raise ValueError('请求数据解析失败, 参数 test_steps:request:headers 格式错误, 必须为 dict 类型')
+
+
+class _StepsSetUpData(BaseModel):
+    testcase: Union[List[Union[dict, str]], None] = None
+    sql: Union[List[Union[dict, str]], None] = None
+    hook: Optional[List[str]] = None
+    wait_time: Optional[int] = None
+
+
+class _StepsTearDownData(BaseModel):
+    sql: Union[List[Union[dict, str]], None] = None
+    hook: Optional[List[str]] = None
+    extract: Optional[List[dict]] = None
+    assert_: Union[List[Union[dict, str]], str, None] = Field(None, alias='assert')
+    wait_time: Optional[int] = None
+
+
+class Steps(BaseModel):
     name: str
     case_id: str
     description: str
-    is_run: Union[bool, str, None] = None
+    is_run: Union[bool, str, None] = ...
+    request: _StepsRequestData
+    setup: Optional[_StepsSetUpData] = None
+    teardown: Optional[_StepsTearDownData] = None
 
 
-class StepsRequestData(BaseModel):
-    method: str
-    url: str
-    params: Union[dict, bytes, None] = None
-    headers: dict
-    data_type: str
-    data: Union[dict, bytes, Tuple[list], None] = None
-    files: Union[List[Dict[Union[list, str]]], None] = None
-
-
-class StepsSetUpData(BaseModel):
-    testcase: Union[str, list, None] = None
-    sql: Optional[list] = None
-    hook: Optional[list] = None
-    wait_time: Optional[int] = None
-
-
-class StepsTearDownData(BaseModel):
-    sql: Optional[list] = None
-    hook: Optional[list] = None
-    extract: Optional[list] = None
-    assert_: Union[str, list, None] = Field(None, alias='assert')
-    wait_time: Optional[int] = None
+class CaseData(BaseModel):
+    config: Config
+    test_steps: Steps
