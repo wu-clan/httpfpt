5a9d38c19bff3022a7d6ce19867f2d85d7f44302
wu <jianhengwu0407@gmail.com>
2022-08-13 20:08:42 +0800

    更新excel请求数据方案

--- fastpt/schema/case_data.py
+++ fastpt/schema/case_data.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from typing import Optional, Union, Tuple, List, Dict, Literal
+from typing import Optional, Union, Tuple, List, Dict, Any, Literal
 
-from pydantic import BaseModel, Field, validator, AnyHttpUrl
+from pydantic import BaseModel, Field, AnyHttpUrl
 
 
-class _ConfigAllureData(BaseModel):
+class ConfigAllureData(BaseModel):
     epic: str
     feature: str
     story: str
 
-    @validator('*')
-    def check_allure_type(cls, v):
-        if not isinstance(v, str):
-            raise ValueError(f'测试用例数据解析失败, 参数 config:allure:{v} 不是有效的 str 类型')
-        return v
 
-
-class _ConfigRequest(BaseModel):
+class ConfigRequestData(BaseModel):
     env: str
     headers: Optional[dict] = None
     timeout: Optional[int] = None
@@ -28,12 +22,12 @@ class _ConfigRequest(BaseModel):
 
 
 class Config(BaseModel):
-    allure: _ConfigAllureData
-    request: _ConfigRequest
+    allure: ConfigAllureData
+    request: ConfigRequestData
     module: str
 
 
-class _StepsRequestData(BaseModel):
+class StepsRequestData(BaseModel):
     method: str
     url: str
     params: Union[dict, bytes, None] = ...
@@ -42,25 +36,45 @@ class _StepsRequestData(BaseModel):
     data: Union[dict, bytes, Tuple[list], None] = ...
     files: Union[Dict[str, Union[List[str], str]], None] = ...
 
-    @validator('headers')
-    def check_headers(cls, v):
-        if v is not None:
-            if not isinstance(v, dict):
-                raise ValueError('请求数据解析失败, 参数 test_steps:request:headers 格式错误, 必须为 dict 类型')
+
+class SetupTestCaseData(BaseModel):
+    case_id: str
+    key: str
+    jsonpath: str
 
 
-class _StepsSetUpData(BaseModel):
-    testcase: Union[List[Union[dict, str]], None] = None
-    sql: Union[List[Union[dict, str]], None] = None
-    hook: Optional[List[str]] = None
+class SetupSqlData(BaseModel):
+    key: str
+    set_type: str
+    sql: str
+    jsonpath: str
+
+
+class StepsSetUpData(BaseModel):
+    testcase: Union[List[Union[SetupTestCaseData, str]], None] = None
+    sql: Union[List[Union[SetupSqlData, str]], None] = None
+    hooks: Optional[List[str]] = None
     wait_time: Optional[int] = None
 
 
-class _StepsTearDownData(BaseModel):
-    sql: Union[List[Union[dict, str]], None] = None
-    hook: Optional[List[str]] = None
-    extract: Optional[List[dict]] = None
-    assert_: Union[List[Union[dict, str]], str, None] = Field(None, alias='assert')
+class TeardownExtractData(BaseModel):
+    key: str
+    set_type: str
+    jsonpath: str
+
+
+class TeardownAssertData(BaseModel):
+    check: str
+    value: Any
+    type: str
+    jsonpath: str
+
+
+class StepsTearDownData(BaseModel):
+    sql: Union[List[Union[SetupSqlData, str]], None] = None
+    hooks: Optional[List[str]] = None
+    extract: Optional[List[TeardownExtractData]] = None
+    assert_: Union[List[Union[TeardownAssertData, str]], str, None] = Field(None, alias='assert')
     wait_time: Optional[int] = None
 
 
@@ -69,11 +83,16 @@ class Steps(BaseModel):
     case_id: str
     description: str
     is_run: Union[bool, str, None] = ...
-    request: _StepsRequestData
-    setup: Optional[_StepsSetUpData] = None
-    teardown: Optional[_StepsTearDownData] = None
+    request: StepsRequestData
+    setup: Optional[StepsSetUpData] = None
+    teardown: Optional[StepsTearDownData] = None
 
 
 class CaseData(BaseModel):
+    """
+    暂不使用此类直接对请求参数做快速校验, 考虑到后期是否对 excel 数据提供支持,
+    如果提供, 那么使用此校验器将会造成 excel 数据错误判断, 除非在 excel 请求数据转化时直接格式化参数请求格式,
+    同时更新和移除请求参数解析中不必要的对 excel 请求数据的二次解析(在此校验器发生之前专门为 excel 请求数据提供的设计)
+    """
     config: Config
     test_steps: Steps
--- fastpt/utils/request/file_data_parse.py
+++ fastpt/utils/request/file_data_parse.py
@@ -33,6 +33,11 @@ def get_request_data(file_data: Union[dict, list]) -> List[dict]:
                     raise ValueError('请求测试用例数据 test_steps 格式错误, 请检查测试用例文件内容')
             return case_list
     # excel
+    # 方案设计:
+    # 1. 简化形式, 将测试用例数据作为 json 格式写入 config 和 test_steps 两列, 非常好实现, 但会导致用例太难维护, 体验极差.
+    # 2. 常规模式, 列设定请求数据对应 config 和 test_steps 下的一级参数, 然后将测试用例数据作为参数要求格式写入多列, 对体验
+    # 会有很大提升, 但仍没有 yaml 使用简便.
+    # excel 支持只是时间问题, 欢迎 PR 方案2, 方案1默认拒绝
     elif isinstance(file_data, list):
         raise ValueError('新版本以暂停对 excel 数据文件的支持')
     else:
