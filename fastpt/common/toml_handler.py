e3a06bba7a331f6623fb69ce024b32ced0f7beb5
wu <jianhengwu0407@gmail.com>
2022-11-17 16:14:32 +0800

    更新依赖包toml为rtoml

--- fastpt/common/toml_handler.py
+++ fastpt/common/toml_handler.py
@@ -2,33 +2,32 @@
 # -*- coding: utf-8 -*-
 import os
 
-import toml
+import rtoml
 
 from fastpt.common.log import log
 
 
-def read_toml(filepath: str, filename: str) -> dict:
+def read_toml(filepath: str, filename: str, encoding: str = 'utf-8') -> dict:
     """
     读取 toml 文件
 
     :param filepath: 文件路径
     :param filename: 文件名
+    :param encoding: 文件内容编码格式
     :return:
     """
     _filename = os.path.join(filepath, filename)
     try:
-        data = toml.load(_filename)
-    except TypeError as e:
-        log.error(f'文件 {filename} 内存在格式错误 \n {e}')
-        raise e
-    except toml.TomlDecodeError as e:
-        log.error(f'文件 {filename} 解析错误')
+        with open(_filename, encoding=encoding) as f:
+            data = rtoml.load(f)
+    except ValueError as e:
+        log.error(f'文件 {filename} 内容在格式错误 \n {e}')
         raise e
     else:
         return data
 
 
-def write_toml(filepath: str, filename: str, data: dict, encoding: str = 'utf-8', mode: str = 'a', encoder=None) -> str:
+def write_toml(filepath: str, filename: str, data: dict, encoding: str = 'utf-8', mode: str = 'a') -> int:
     """
     将字典写入包含 TOML 格式数据的文件
 
@@ -37,7 +36,6 @@ def write_toml(filepath: str, filename: str, data: dict, encoding: str = 'utf-8'
     :param data: 写入内容
     :param encoding: 文件内容编码格式
     :param mode: 文件写入模式
-    :param encoder: TOML 编码器
     :return:
     """
     if not os.path.exists(filepath):
@@ -46,8 +44,8 @@ def write_toml(filepath: str, filename: str, data: dict, encoding: str = 'utf-8'
     _file = os.path.join(filepath, filename)
     try:
         with open(_file, encoding=encoding, mode=mode) as f:
-            result = toml.dump(data, f, encoder=encoder)
-    except TypeError as e:
+            result = rtoml.dump(data, f)
+    except ValueError as e:
         log.error(f'写入文件 "{filename}" 错误 \n {e}')
         raise e
     else:
