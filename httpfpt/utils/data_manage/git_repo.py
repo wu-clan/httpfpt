c002e33c5e3916001a979ebfdb6b40af1eb0c0ce
Wu Clan <jianhengwu0407@gmail.com>
2023-06-13 12:21:09 +0800

    添加在线测试用例拉取后自动检测数据结构

--- httpfpt/utils/data_manage/git_repo.py
+++ httpfpt/utils/data_manage/git_repo.py
@@ -4,6 +4,12 @@ import os
 import shutil
 
 import typer
+from pydantic import ValidationError
+
+from httpfpt.common.yaml_handler import read_yaml
+from httpfpt.schemas.case_data import CaseData
+from httpfpt.utils.file_control import search_all_case_yaml_files
+from httpfpt.utils.pydantic_error_parse import parse_error
 
 
 class GitRepoPaser:
@@ -20,16 +26,30 @@ class GitRepoPaser:
             raise ValueError('git 仓库克隆地址错误, 请使用 https 地址')
 
         try:
-            online_dir = os.path.join(os.path.abspath('./data'), 'online')
-            typer.echo(online_dir)
-            if os.path.exists(online_dir):
-                shutil.rmtree(online_dir)
-            os.makedirs(online_dir)
-            result = os.system(f'cd {online_dir} && git clone {src}')
+            online_dir_path = os.path.join(os.path.abspath('./data/test_data'), 'online_yaml_data')
+            typer.echo(online_dir_path)
+            if os.path.exists(online_dir_path):
+                shutil.rmtree(online_dir_path)
+            os.makedirs(online_dir_path)
+            result = os.system(f'cd {online_dir_path} && git clone {src}')
         except Exception as e:
-            raise RuntimeError(f'❌ git 苍鹭测试数据拉取失败：{e}')
+            raise RuntimeError(f'git 苍鹭测试数据拉取失败：{e}')
         else:
             if result == 0:
                 typer.secho('✅ git 仓库数据文件拉取成功')
             else:
-                raise RuntimeError('❌ git 仓库测试数据拉取失败')
+                raise RuntimeError('git 仓库测试数据拉取失败')
+
+        all_yaml_file = search_all_case_yaml_files(online_dir_path)
+        all_case_data = []
+        for file in all_yaml_file:
+            read_data = read_yaml(None, filename=file)
+            all_case_data.append(read_data)
+        for case_data in all_case_data:
+            try:
+                count: int = 0
+                CaseData.model_validate(case_data, strict=True)
+            except ValidationError as e:
+                count = parse_error(e)
+            if count > 0:
+                raise ValueError(f'用例数据校验失败，共有 {count} 处错误, 错误详情请查看日志')
