1957c1d95ad251ba88d860029be930cf9942edeb
Wu Clan <jianhengwu0407@gmail.com>
2024-03-04 19:09:19 +0800

    添加执行脚本

--- httpfpt/__init__.py
+++ httpfpt/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from httpfpt.core.get_conf import set_project_config as set_project_config
+from httpfpt.core.path_conf import set_project_dir as set_project_dir
 
 __version__ = 'v0.5.1'
