40e3b89f976eb0fe5e43ef67486dc72640786770
Wu Clan <jianhengwu0407@gmail.com>
2024-08-29 11:47:24 +0800

    更新测试启动项配置 (#199)
    * 修复allure拷贝文件逻辑
    
    * 更新测试启动项配置

--- httpfpt/utils/cli/version.py
+++ httpfpt/utils/cli/version.py
@@ -9,11 +9,13 @@ import cappa
 from httpfpt.utils.rich_console import console
 
 
-def get_version() -> None:
+def get_version(cli: bool = True) -> None:
     """获取版本号"""
     ver = read_text('httpfpt', '__init__.py')
     mob = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", ver, re.MULTILINE)
     if mob:
-        console.print(f'\n🔥 HttpFpt [cyan]{mob.group(1)}[/]')
+        if cli:
+            console.print('')
+        console.print(f'HTTPFPT: [cyan]{mob.group(1)}[/]')
     else:
         raise cappa.Exit('\n❌ 未查询到版本号', code=1)
